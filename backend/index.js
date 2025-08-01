5d327a61bb40af4e2d813a5d0208f2cb9359f618
krish <dhandhukiya482.com>
2025-08-01 12:19:11 +0530

    added

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,42 @@
+// const express = require("express");
+// const cors = require("cors");
+// const conn = require("./utilities/connectdb")
+// const userrouter = require("./routes/use_routes")
+// const connectCloudinary = require("./utilities/cloudinary");
+// const productRouter = require("./routes/productRouter");
+// const cartRouter = require("./routes/cartRouter");
+// const orderRouter = require("./routes/orderRoute");
+
+// const app = express();
+// app.use(cors());
+// app.use(express.json());
+// connectCloudinary()
+
+// app.use('/api', userrouter)
+// app.use('/api/product', productRouter)
+// app.use('/api/cart', cartRouter)
+// app.use('/api/order', orderRouter)
+
+
+
+// const startserver = async () => {
+//     try {
+//         await conn()
+//         app.listen(1000, () => {
+//             console.log('Server is starting on the port 1000')
+//         })
+//     }
+//     catch (error) {
+//         console.log("failed to start server.", error);
+//     }
+// }
+
+// startserver()
+
 const express = require("express");
 const cors = require("cors");
-const conn = require("./utilities/connectdb")
-const userrouter = require("./routes/use_routes")
+const conn = require("./utilities/connectdb");
+const userrouter = require("./routes/use_routes");
 const connectCloudinary = require("./utilities/cloudinary");
 const productRouter = require("./routes/productRouter");
 const cartRouter = require("./routes/cartRouter");
@@ -10,23 +45,28 @@ const orderRouter = require("./routes/orderRoute");
 const app = express();
 app.use(cors());
 app.use(express.json());
-connectCloudinary()
 
-app.use('/api', userrouter)
-app.use('/api/product', productRouter)
-app.use('/api/cart', cartRouter)
-app.use('/api/order', orderRouter)
+connectCloudinary();
+conn();
 
-const startserver = async () => {
-    try {
-        await conn()
-        app.listen(1000, () => {
-            console.log('Server is starting on the port 1000')
-        })
-    }
-    catch (error) {
-        console.log("failed to start server.", error);
-    }
+app.use("/api", userrouter);
+app.use("/api/product", productRouter);
+app.use("/api/cart", cartRouter);
+app.use("/api/order", orderRouter);
+
+app.get("/", (req, res) => {
+  res.send("Backend is working");
+});
+
+if (process.env.NODE_ENV !== "production") {
+  const PORT = 1000;
+  app.listen(PORT, () => {
+    console.log(`Local server running on port ${PORT}`);
+  });
+}
+else
+{
+    console.log("failed to start server");
 }
 
-startserver()
+module.exports = app;
